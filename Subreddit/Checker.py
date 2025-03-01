79f95831d3a7e354ceb0ac3ca94d542ffbd17b1e
Sam Button <sam.button@outlook.co.nz>
2025-03-01 13:19:25 +1300

    remove sensitive data by using a cofig file

--- Subreddit/Checker.py
+++ Subreddit/Checker.py
@@ -5,15 +5,15 @@ import praw.exceptions
 # and the puts them into a List to iterate over as well as makes file of all the names
 def check_subreddit(reddit: praw.Reddit, search_terms: list[str]):
     
-    foundlist = []
+    foundSet = set()
     writefile = open('founditems.txt', 'w')
     for item in search_terms:
         findingSubreddits = reddit.subreddits.search(query=item)
         for i in findingSubreddits:
             if i.subreddit_type != 'private':
-                foundlist.append(i.display_name)
+                foundSet.add(i.display_name)
                 writefile.write(f'{i.display_name}\n')
                 
     writefile.flush
     writefile.close
-    return foundlist
+    return foundSet
