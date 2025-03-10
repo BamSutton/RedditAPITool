fd87b1e3c472e1bf4683cb7a97aaaecce7c63dfe
Sam Button <sam.button@outlook.co.nz>
2025-03-10 22:41:13 +1100

    add models in for each of the types of items that are being collected

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import praw
 from Subreddit.Checker import check_subreddit
 import config
+from CSV_creator import createCSVs
+from models import Subreddits, Submissions, Comments
 
 import praw.exceptions
 
@@ -15,23 +17,65 @@ reddit = praw.Reddit(
     username=config.username,
 )
 # these are the search terms that we care about currently
-# they get feed into the below function "check_subreddit" which finds all of the relevant subreddits (see Checker.py)
+# they get fed into the below function "check_subreddit" which finds all of the relevant subreddits (see Checker.py)
 search_terms = ['Autism','neurodivergent','ASD','neurospicy']
-foundlist = check_subreddit(reddit=reddit,search_terms=search_terms)
+foundSubredditList = check_subreddit(reddit,search_terms)
 
-redditResultList = []
 # this loops through all of the recently found subreddits from "check_subreddit" and searches for all the top posts
 # across all time with the query term "unmasking" and prints out the URL to that post into the console log
-for subreddit in foundlist:
-    print(subreddit)
-    postingDict = {}
-    #for posting in reddit.subreddit(subreddit).search("unmasking", sort='top',time_filter='all', limit= None):
-    for posting in reddit.subreddit(subreddit).search("unmasking", sort='top',time_filter='all', limit= 1):
-        posting.comments.replace_more(limit=0)
-        commentDict = {}
-        for comment in posting.comments:
-            commentDict[posting.name] = comment.body
-        print(f"{commentDict}")
-        #postingDict[subreddit] = commentDict
-    #redditResultList.append(postingDict)
-#print(redditResultList)
+subredditListData: list[Subreddits] = []
+count = 0
+for subredditname in foundSubredditList:
+    count += 1
+    subreddit = reddit.subreddit(subredditname)
+    print(f"{count}. --Pulling {subredditname} data--")
+    newsubreddit = Subreddits(
+        id = subreddit.id,
+        name = subreddit.display_name,
+        description= subreddit.description,
+        time_created= subreddit.created_utc,
+        subsrcibers= subreddit.subscribers,
+    )
+    #for submission in subreddit.search("unmasking", sort='top',time_filter='all', limit= None):
+    for submission in subreddit.search("unmasking", sort='top',time_filter='all', limit= 1):
+        newSubmission = Submissions(
+            id = submission.id, 
+            title=submission.title, 
+            content=submission.selftext,
+            score = submission.score,
+            poster = submission.author.name if submission.author else "Deleted", 
+            time_created=submission.created_utc,)
+        
+        submission.comments.replace_more(limit=None)
+        for comment in submission.comments.list():
+            newComment = Comments(
+                id=comment.id,
+                content=comment.body,
+                poster=comment.author.name if comment.author else "Deleted",
+                score=comment.score,
+                time_created=comment.created_utc,
+            )
+            newSubmission.commentList.append(newComment)
+        newsubreddit.submissionList.append(newSubmission)
+    # print (f"Added {(newsubreddit.submissionList)} posts to subredit: {newsubreddit.name}")
+    subredditListData.append(newsubreddit)
+for s in subredditListData:
+    print (f"{s.name} post count:{len(s.submissionList)}")
+    for sub in s.submissionList:
+        print (f'Number of Comments: {len(sub.commentList)}')
+
+# createCSVs()
+
+
+# print(subredditList)
+
+# def csv_comment_formatter(comment_list):
+
+#     return None
+
+
+# -subreddit-
+# post1-title-selftext    comment1    comment1.1
+#                                     comment1.2
+#                                                 comment1.2.1
+#                         comment2    comment2.1
---
+++ models.py
@@ -0,0 +1,69 @@
+from datetime import datetime
+
+class Comments:
+    id: str
+    redditor: str
+    content: str
+    score: int
+    children: list
+    time_created: datetime
+
+    def __init__(self, 
+                 id: str, 
+                 poster: str,
+                 content: str, 
+                 score: int, 
+                 time_created: int):
+        
+        self.id = id
+        self.poster = poster
+        self.content = content
+        self.score = score
+        self.children = []
+        self.time_created = datetime.fromtimestamp(time_created)
+
+class Submissions:
+    id: str
+    title: str
+    content: str
+    poster: str
+    score: int
+    time_created: datetime
+    commentList: list
+
+    def __init__(self, id: str,
+                  title: str, 
+                  content: str,
+                  poster: str,
+                  score: int, 
+                  time_created: int):
+        
+        self.id = id
+        self.title = title
+        self.content = content
+        self.poster = poster
+        self.score = score
+        self.time_created = datetime.fromtimestamp(time_created)
+        self.commentList = []
+
+class Subreddits:
+    id: str
+    name: str
+    description: str
+    time_created: datetime
+    subscribers: int
+    submissionList: list
+
+    def __init__(self, 
+                 id: str, 
+                 name: str, 
+                 description: str, 
+                 time_created: int, 
+                 subsrcibers: int):
+        
+        self.id = id
+        self.name = name
+        self.description = description
+        self.time_created = datetime.fromtimestamp(time_created)
+        self.subscribers = subsrcibers
+        self.submissionList = []
