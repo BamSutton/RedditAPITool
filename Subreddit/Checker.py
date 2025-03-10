6d778d8f565a6c8c7baefb5c403a59aeef1433ad
Sam Button <sam.button@outlook.co.nz>
2025-03-10 22:44:53 +1100

    add check to make sure the subreddits that we are using are not repeated

--- Subreddit/Checker.py
+++ Subreddit/Checker.py
@@ -4,16 +4,20 @@ import praw.exceptions
 # this pulls all of the subreddits that match the search terms provided
 # and the puts them into a List to iterate over as well as makes file of all the names
 def check_subreddit(reddit: praw.Reddit, search_terms: list[str]):
-    
+    print(f"pulling subreddit data from search terms: {search_terms}")
     foundSet = set()
+    count = 0
     writefile = open('founditems.txt', 'w')
-    for item in search_terms:
-        findingSubreddits = reddit.subreddits.search(query=item)
-        for i in findingSubreddits:
-            if i.subreddit_type != 'private':
-                foundSet.add(i.display_name)
-                writefile.write(f'{i.display_name}\n')
-                
+    for term in search_terms:
+        findingSubreddits = reddit.subreddits.search(query=term,limit = 1) # this searches for subreddits based on a search term
+        for item in findingSubreddits:
+            # This filters out private subreddits and subreddits that have already been collected
+            if item.subreddit_type != 'private' and item.display_name not in foundSet: 
+                foundSet.add(item.display_name)
+                writefile.write(f'{item.display_name}\n')
+            else:
+                count+=1
+    print(f"Total unique subreddist's pulled: {len(foundSet)}")            
     writefile.flush
     writefile.close
-    return foundSet
+    return foundSet, count
