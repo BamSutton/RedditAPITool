0c78aaaa489c47172449c79e904653f281d80517
Sam Button <sam.button@outlook.co.nz>
2025-03-19 15:35:29 +1100

    add parent id back in implement a default list to hold the comment structure for each post

--- models.py
+++ models.py
@@ -1,11 +1,12 @@
 from datetime import datetime
+from collections import defaultdict
 
 class Comments:
     id: str
     redditor: str
     content: str
     score: int
-    children: list
+    parent_id: str
     time_created: datetime
 
     def __init__(self, 
@@ -13,13 +14,14 @@ class Comments:
                  poster: str,
                  content: str, 
                  score: int, 
+                 parent_id: str,
                  time_created: int):
         
         self.id = id
         self.poster = poster
         self.content = content
         self.score = score
-        self.children = []
+        self.parent_id = parent_id
         self.time_created = datetime.fromtimestamp(time_created)
 
 class Submissions:
@@ -30,6 +32,7 @@ class Submissions:
     score: int
     time_created: datetime
     commentList: list
+    commentMap: defaultdict
 
     def __init__(self, id: str,
                   title: str, 
@@ -45,6 +48,7 @@ class Submissions:
         self.score = score
         self.time_created = datetime.fromtimestamp(time_created)
         self.commentList = []
+        self.commentMap = defaultdict(list)
 
 class Subreddits:
     id: str
