318522c1726f7eb0b02aa8023cf847f98569c438
Sam Button <sam.button@outlook.co.nz>
2025-05-23 16:20:53 +1000

    adds in base template for reddit connection informtion

---
+++ config.py
@@ -0,0 +1,7 @@
+# Client id and secret from your app that was created in Reddit goes here
+client_id = ''
+client_secret = ''
+
+#the reddit account information that created the above app goes here
+username = ''
+password = ''
