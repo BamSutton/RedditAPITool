ae885c5fd91c1007ef7327fba8b6b7be4a494977
Sam Button <sam.button@outlook.co.nz>
2025-05-23 15:45:53 +1000

    - Updates inputs and outputs of create sample functions to use dict of dataframes for readability - Added comments to describe functions purpose

--- SampleSelector.py
+++ SampleSelector.py
@@ -1,14 +1,19 @@
 import pandas as pd
 
-def create_new_sample(df_subreddits: pd.DataFrame, df_submissions: pd.DataFrame, df_comments: pd.DataFrame):
+"""
+Given the dictionary of dataframes provided and the sample size specified,
+Return a Sample of the data centred around submissions provided, with the corrosponding subreddits 
+and comments that these submissions are linked to.
+
+This returns a dictionary of the dataframes created from the sampling functions 
+"""
+def create_new_samples(dataframeDict: dict, sample_size: float):
     
-    submission_sample_df = df_submissions.sample(frac=0.05, replace=False, random_state=1)
-    #submission_sample_df.set_index('ID',inplace=True)
-    comment_mask = df_comments['Submission_ID'].isin(submission_sample_df['ID'])
-    comment_sample_df = df_comments[comment_mask]
-    print (f'Sample of Submissions: \n{submission_sample_df}')
-    print (df_submissions)
-    print (f'Sample of Comments: \n{comment_sample_df}')
-    print (df_comments)
+    sample_df_submission = dataframeDict("submissions").sample(frac=sample_size, replace=False, random_state=1)
+    comment_mask = dataframeDict("comments")['Submission_ID'].isin(sample_df_submission['ID'])
+    sample_df_comment = dataframeDict("comments")[comment_mask]
+    subreddit_mask = dataframeDict("subreddits")['ID'].isin(sample_df_submission['Subreddit_ID'])
+    sample_df_subreddit = dataframeDict("subreddits")[subreddit_mask]
     
-    # df1[df1.index.isin(df2.index)]
+    return {"subreddits": sample_df_subreddit, "submissions": sample_df_submission, "comments": sample_df_comment,}
+    
