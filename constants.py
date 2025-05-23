01bc957747347f0daf08e5cea94ae5174fb4cb79
Sam Button <sam.button@outlook.co.nz>
2025-05-23 15:40:57 +1000

    Moves common information into one location

--- constants.py
+++ constants.py
@@ -1,3 +1,8 @@
+# data frame structure that is used through the App
 subreddit_dict = {"ID":[],"Name":[],"Subscribers":[],"Description":[],"Created":[]}
 submission_dict = {"ID":[],"Title":[],"Content":[],"Author":[],"Score":[],"Created":[], "Subreddit_ID":[]}
 comment_dict = {"ID":[],"Content":[],"Author":[],"Score":[],"Created":[],"Parent_ID":[],"Submission_ID":[]}
+
+# base data that can be changed if new inputs are required
+search_terms = ["Autism"]
+sample_size = 0.05
